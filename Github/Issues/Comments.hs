918112b13ab8797a3a569cd067672eccea36b9f3
Mike Burns <mike@mike-burns.com>
2011-11-10 21:31:45 -0500

    Github.Issues.Comments.comments - all comments on an Issue.

--- Github/Issues/Comments.hs
+++ Github/Issues/Comments.hs
@@ -1,5 +1,6 @@
 module Github.Issues.Comments (
  comment
+,comments
 ,module Github.Data
 ) where
 
@@ -9,3 +10,7 @@ import Github.Private
 comment :: String -> String -> Int -> IO (Either Error IssueComment)
 comment user repoName commentId =
   githubGet ["repos", user, repoName, "issues", "comments", show commentId]
+
+comments :: String -> String -> Int -> IO (Either Error [IssueComment])
+comments user repoName issueNumber =
+  githubGet ["repos", user, repoName, "issues", show issueNumber, "comments"]
---
+++ samples/Issues/Comments/ShowComments.hs
@@ -0,0 +1,17 @@
+module ShowComments where
+
+import qualified Github.Issues.Comments as Github
+import Data.List (intercalate)
+
+main = do
+  possibleComments <- Github.comments "thoughtbot" "paperclip" 635
+  case possibleComments of
+       (Left error) -> putStrLn $ "Error: " ++ show error
+       (Right issues) ->
+         putStrLn $ intercalate "\n\n" $ map formatComment issues
+
+formatComment comment =
+  (Github.githubUserLogin $ Github.issueCommentUser comment) ++
+    " commented " ++
+    (show $ Github.fromGithubDate $ Github.issueCommentUpdatedAt comment) ++
+    "\n" ++ (Github.issueCommentBody comment)
