eed831051496531c09d1b429d8ccc93c76d7e3eb
Mike Burns <mike@mike-burns.com>
2011-11-10 21:09:40 -0500

    Github.Issues.issuesForRepo along with the IssueLimitation data type for limiting which issues are produced.

--- Github/Issues.hs
+++ Github/Issues.hs
@@ -1,12 +1,55 @@
 module Github.Issues (
  issue
+,issuesForRepo
+,IssueLimitation(..)
 ,module Github.Data
 ) where
 
 import Github.Data
 import Github.Private
+import Data.List (intercalate)
+import Data.Time.Format (formatTime)
+import System.Locale (defaultTimeLocale)
+import Data.Time.Clock (UTCTime(..))
+
+data IssueLimitation =
+      AnyMilestone
+    | NoMilestone
+    | MilestoneId Int
+    | Open
+    | Closed
+    | Unassigned
+    | AnyAssignment
+    | AssignedTo String
+    | Mentions String
+    | Labels [String]
+    | Ascending
+    | Descending
+    | Since UTCTime
 
 issue :: String -> String -> Int -> IO (Either Error Issue)
 issue user repoName issueNumber =
   githubGet ["repos", user, repoName, "issues", show issueNumber]
 
+issuesForRepo :: String -> String -> [IssueLimitation] -> IO (Either Error [Issue])
+issuesForRepo user repoName issueLimitations =
+  githubGetWithQueryString
+    ["repos", user, repoName, "issues"]
+    (queryStringFromLimitations issueLimitations)
+  where
+    queryStringFromLimitations = intercalate "&" . map convert
+
+    convert AnyMilestone     = "milestone=*"
+    convert NoMilestone      = "milestone=none"
+    convert (MilestoneId n)  = "milestone=" ++ show n
+    convert Open             = "state=open"
+    convert Closed           = "state=closed"
+    convert Unassigned       = "assignee=none"
+    convert AnyAssignment    = "assignee=*"
+    convert (AssignedTo u)   = "assignee=" ++ u
+    convert (Mentions u)     = "mentioned=" ++ u
+    convert (Labels l)       = "labels=" ++ intercalate "," l
+    convert Ascending        = "direction=asc"
+    convert Descending       = "direction=desc"
+    convert (Since t)        =
+      "since=" ++ formatTime defaultTimeLocale "%FT%TZ" t
---
+++ samples/Issues/ShowRepoIssues.hs
@@ -0,0 +1,21 @@
+module ShowRepoIssue where
+
+import qualified Github.Issues as Github
+import Data.List (intercalate)
+
+main = do
+  let limitations = [Github.Closed, Github.Mentions "mike-burns", Github.AssignedTo "jyurek"]
+  possibleIssues <- Github.issuesForRepo "thoughtbot" "paperclip" limitations
+  case possibleIssues of
+       (Left error) -> putStrLn $ "Error: " ++ show error
+       (Right issues) ->
+         putStrLn $ intercalate "\n\n" $ map formatIssue issues
+
+formatIssue issue =
+  (Github.githubUserLogin $ Github.issueUser issue) ++
+    " opened this issue " ++
+    (show $ Github.fromGithubDate $ Github.issueCreatedAt issue) ++ "\n" ++
+    (Github.issueState issue) ++ " with " ++
+    (show $ Github.issueComments issue) ++ " comments" ++ "\n\n" ++
+    (Github.issueTitle issue)
+
