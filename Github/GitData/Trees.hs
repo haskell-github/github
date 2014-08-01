b5c749e496708fc28e0c9fb98c50c7d757000af4
Charles O'Farrell <charleso@charleso.org>
2014-08-02 07:28:47 +1000

    Add extra auth functions for Trees

--- Github/GitData/Trees.hs
+++ Github/GitData/Trees.hs
@@ -11,15 +11,27 @@ import Github.Private
 
 -- | A tree for a SHA1.
 --
+-- > tree (Just ("github-username", "github-password")) "thoughtbot" "paperclip" "fe114451f7d066d367a1646ca7ac10e689b46844"
+tree' :: Maybe GithubAuth -> String -> String -> String -> IO (Either Error Tree)
+tree' auth user reqRepoName sha =
+  githubGet' auth ["repos", user, reqRepoName, "git", "trees", sha]
+
+-- | A tree for a SHA1.
+--
 -- > tree "thoughtbot" "paperclip" "fe114451f7d066d367a1646ca7ac10e689b46844"
 tree :: String -> String -> String -> IO (Either Error Tree)
-tree user reqRepoName sha =
-  githubGet ["repos", user, reqRepoName, "git", "trees", sha]
+tree = tree' Nothing
+
+-- | A recursively-nested tree for a SHA1.
+--
+-- > nestedTree' (Just ("github-username", "github-password")) "thoughtbot" "paperclip" "fe114451f7d066d367a1646ca7ac10e689b46844"
+nestedTree' :: Maybe GithubAuth -> String -> String -> String -> IO (Either Error Tree)
+nestedTree' auth user reqRepoName sha =
+  githubGetWithQueryString' auth ["repos", user, reqRepoName, "git", "trees", sha]
+                                 "recursive=1"
 
 -- | A recursively-nested tree for a SHA1.
 --
 -- > nestedTree "thoughtbot" "paperclip" "fe114451f7d066d367a1646ca7ac10e689b46844"
 nestedTree :: String -> String -> String -> IO (Either Error Tree)
-nestedTree user reqRepoName sha =
-  githubGetWithQueryString ["repos", user, reqRepoName, "git", "trees", sha]
-                           "recursive=1"
+nestedTree = nestedTree' Nothing
