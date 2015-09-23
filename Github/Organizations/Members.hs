1c770f54ef19e4ebab5195803f2b137b11ae1059
Andrew Darqui <andrew.darqui@gmail.com>
2015-09-22 22:40:40 -0400

    Support authenticated requests for the members of an organization.

--- Github/Organizations/Members.hs
+++ Github/Organizations/Members.hs
@@ -2,14 +2,23 @@
 -- <http://developer.github.com/v3/orgs/members/>.
 module Github.Organizations.Members (
  membersOf
+,membersOf'
 ,module Github.Data
 ) where
 
 import Github.Data
 import Github.Private
 
--- | All the users who are members of the specified organization.
+-- | All the users who are members of the specified organization,
+-- | with or without authentication.
+--
+-- > membersOf' (Just $ GithubOAuth "token") "thoughtbot"
+membersOf' :: Maybe GithubAuth -> String -> IO (Either Error [GithubOwner])
+membersOf' auth organization = githubGet' auth ["orgs", organization, "members"]
+
+-- | All the users who are members of the specified organization,
+-- | without authentication.
 --
 -- > membersOf "thoughtbot"
 membersOf :: String -> IO (Either Error [GithubOwner])
-membersOf organization = githubGet ["orgs", organization, "members"]
+membersOf = membersOf' Nothing
