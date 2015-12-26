b3c3d4682f4ceb5ba92a83452814478ffb830fc5
Oleg Grenrus <oleg.grenrus@iki.fi>
2015-12-26 14:31:05 +0200

    Add tagged name

---
+++ Github/Data/Name.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric      #-}
+module Github.Data.Name (
+    Name(..),
+    mkName,
+    untagName,
+    ) where
+
+import Control.DeepSeq   (NFData(..))
+import Data.Aeson.Compat (FromJSON(..), ToJSON(..))
+import Data.Data         (Data, Typeable)
+import Data.Hashable     (Hashable)
+import Data.String       (IsString(..))
+import GHC.Generics      (Generic)
+
+newtype Name entity = N String
+    deriving (Eq, Ord, Show, Read, Generic, Typeable, Data)
+
+-- | Smart constructor for 'Name'
+mkName :: proxy entity -> String -> Name entity
+mkName _ = N
+
+untagName :: Name entity -> String
+untagName (N name) = name
+
+instance Hashable (Name entity)
+
+instance NFData (Name entity) where
+    rnf (N s) = rnf s
+
+instance FromJSON (Name entity) where
+    parseJSON = fmap N . parseJSON
+
+instance ToJSON (Name entity) where
+    toJSON = toJSON . untagName
+
+instance IsString (Name entity) where
+    fromString = N
