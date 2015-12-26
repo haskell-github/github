f3b837195513ac14c50a0919992b3db8801c1da1
Volodymyr Shatsky <shockone89@gmail.com>
2015-12-27 01:04:10 +0200

    Add missing functions to the Issues API.

--- Github/Issues/Labels.hs
+++ Github/Issues/Labels.hs
@@ -2,29 +2,126 @@
 -- | The API for dealing with labels on Github issues, as described on
 -- <http://developer.github.com/v3/issues/labels/>.
 module Github.Issues.Labels (
- label
-,labelsOnRepo
-,labelsOnIssue
-,labelsOnMilestone
+ labelsOnRepo
+,labelsOnRepo'
+,label
+,label'
 ,createLabel
+,updateLabel
+,deleteLabel
+,labelsOnIssue
+,labelsOnIssue'
+,addLabelsToIssue
+,removeLabelFromIssue
+,replaceAllLabelsForIssue
+,removeAllLabelsFromIssue
+,labelsOnMilestone
+,labelsOnMilestone'
 ,module Github.Data
 ) where
 
-import           Data.Aeson     (object, (.=))
-import           Github.Data
-import           Github.Private
+import Data.Aeson     (object, (.=))
+import Github.Data
+import Github.Private
+
 -- | All the labels available to use on any issue in the repo.
 --
 -- > labelsOnRepo "thoughtbot" "paperclip"
 labelsOnRepo :: String -> String -> IO (Either Error [IssueLabel])
 labelsOnRepo user reqRepoName = githubGet ["repos", user, reqRepoName, "labels"]
 
+-- | All the labels available to use on any issue in the repo, using authentication.
+--
+-- > labelsOnRepo' (GithubUser (user, password)) "thoughtbot" "paperclip"
+labelsOnRepo' :: GithubAuth -> String -> String -> IO (Either Error [IssueLabel])
+labelsOnRepo' auth user reqRepoName =
+  githubGet' (Just auth) ["repos", user, reqRepoName, "labels"]
+
+-- | A label, by name.
+--
+-- > label "thoughtbot" "paperclip" "bug"
+label :: String -> String -> String -> IO (Either Error IssueLabel)
+label user reqRepoName reqLabelName =
+  githubGet ["repos", user, reqRepoName, "labels", reqLabelName]
+
+-- | A label, by name, using authentication.
+--
+-- > label' (GithubUser (user, password)) "thoughtbot" "paperclip" "bug"
+label' :: GithubAuth -> String -> String -> String -> IO (Either Error IssueLabel)
+label' auth user reqRepoName reqLabelName =
+  githubGet' (Just auth) ["repos", user, reqRepoName, "labels", reqLabelName]
+
+-- | Create a label
+--
+-- > createLabel (GithubUser (user, password)) "thoughtbot" "paperclip" "bug" "f29513"
+createLabel :: GithubAuth -> String -> String -> String -> String -> IO (Either Error IssueLabel)
+createLabel auth reqUserName reqRepoName reqLabelName reqLabelColor = githubPost auth paths body
+  where
+    paths = ["repos", reqUserName, reqRepoName, "labels"]
+    body = object ["name" .= reqLabelName, "color" .= reqLabelColor]
+
+-- | Update a label
+--
+-- > updateLabel (GithubUser (user, password)) "thoughtbot" "paperclip" "bug" "new-bug" "ff1111"
+updateLabel :: GithubAuth -> String -> String -> String -> String -> String -> IO (Either Error IssueLabel)
+updateLabel auth reqUserName reqRepoName oldLabelName newLabelName reqLabelColor = githubPatch auth paths body
+  where
+    paths = ["repos", reqUserName, reqRepoName, "labels", oldLabelName]
+    body = object ["name" .= newLabelName, "color" .= reqLabelColor]
+
+-- | Delete a label
+--
+-- > deleteLabel (GithubUser (user, password)) "thoughtbot" "paperclip" "bug"
+deleteLabel :: GithubAuth -> String -> String -> String -> IO (Either Error ())
+deleteLabel auth reqUserName reqRepoName reqLabelName = githubDelete auth paths
+  where
+    paths = ["repos", reqUserName, reqRepoName, "labels", reqLabelName]
+
 -- | The labels on an issue in a repo.
 --
 -- > labelsOnIssue "thoughtbot" "paperclip" 585
-labelsOnIssue :: String -> String -> Int ->  IO (Either Error [IssueLabel])
+labelsOnIssue :: String -> String -> Int -> IO (Either Error [IssueLabel])
 labelsOnIssue user reqRepoName reqIssueId =
   githubGet ["repos", user, reqRepoName, "issues", show reqIssueId, "labels"]
+
+-- | The labels on an issue in a repo, using authentication.
+--
+-- > labelsOnIssue' (GithubUser (user, password)) "thoughtbot" "paperclip" 585
+labelsOnIssue' :: GithubAuth -> String -> String -> Int -> IO (Either Error [IssueLabel])
+labelsOnIssue' auth user reqRepoName reqIssueId =
+  githubGet' (Just auth) ["repos", user, reqRepoName, "issues", show reqIssueId, "labels"]
+
+-- | Add labels to an issue.
+--
+-- > addLabelsToIssue (GithubUser (user, password)) "thoughtbot" "paperclip" 585 ["Label1", "Label2"]
+addLabelsToIssue :: GithubAuth -> String -> String -> Int -> [String] -> IO (Either Error [IssueLabel])
+addLabelsToIssue auth user reqRepoName reqIssueId = githubPost auth paths
+  where
+    paths =["repos", user, reqRepoName, "issues", show reqIssueId, "labels"]
+
+-- | Remove a label from an issue.
+--
+-- > removeLabelFromIssue (GithubUser (user, password)) "thoughtbot" "paperclip" 585 "bug"
+removeLabelFromIssue :: GithubAuth -> String -> String -> Int -> String -> IO (Either Error ())
+removeLabelFromIssue auth user reqRepoName reqIssueId reqLabelName = githubDelete auth paths
+  where
+    paths =["repos", user, reqRepoName, "issues", show reqIssueId, "labels", reqLabelName]
+
+-- | Replace all labels on an issue. Sending an empty list will remove all labels from the issue.
+--
+-- > replaceAllLabelsForIssue (GithubUser (user, password)) "thoughtbot" "paperclip" 585 ["Label1", "Label2"]
+replaceAllLabelsForIssue :: GithubAuth -> String -> String -> Int -> [String] -> IO (Either Error [IssueLabel])
+replaceAllLabelsForIssue auth user reqRepoName reqIssueId = githubPut auth paths
+  where
+    paths =["repos", user, reqRepoName, "issues", show reqIssueId, "labels"]
+
+-- | Remove all labels from an issue.
+--
+-- > removeAllLabelsFromIssue (GithubUser (user, password)) "thoughtbot" "paperclip" 585
+removeAllLabelsFromIssue :: GithubAuth -> String -> String -> Int -> IO (Either Error ())
+removeAllLabelsFromIssue auth user reqRepoName reqIssueId = githubDelete auth paths
+  where
+    paths =["repos", user, reqRepoName, "issues", show reqIssueId, "labels"]
 
 -- | All the labels on a repo's milestone, given the milestone ID.
 --
@@ -33,22 +130,9 @@ labelsOnMilestone :: String -> String -> Int ->  IO (Either Error [IssueLabel])
 labelsOnMilestone user reqRepoName milestoneId =
   githubGet ["repos", user, reqRepoName, "milestones", show milestoneId, "labels"]
 
--- | A label, by name.
+-- | All the labels on a repo's milestone, given the milestone ID, using authentication.
 --
--- > Github.label "thoughtbot" "paperclip" "bug"
-label :: String -> String -> String -> IO (Either Error IssueLabel)
-label user reqRepoName reqLabelName =
-  githubGet ["repos", user, reqRepoName, "labels", reqLabelName]
--- | Create a label
-
-createLabel :: GithubAuth
-            -> String
-            -> String
-            -> String
-            -> String
-            -> IO (Either Error IssueLabel)
-createLabel auth reqUserName reqRepoName reqLabelName reqLabelColor = githubPost auth paths body
-  where
-    paths = ["repos", reqUserName, reqRepoName, "labels"]
-    body = object ["name" .= reqLabelName
-                  ,"color" .= reqLabelColor]
+-- > labelsOnMilestone' (GithubUser (user, password)) "thoughtbot" "paperclip" 2
+labelsOnMilestone' :: GithubAuth -> String -> String -> Int ->  IO (Either Error [IssueLabel])
+labelsOnMilestone' auth user reqRepoName milestoneId =
+  githubGet' (Just auth) ["repos", user, reqRepoName, "milestones", show milestoneId, "labels"]
