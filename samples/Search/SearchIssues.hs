04816293789c0db26378ea0249f0beabfe26ec41
Daniel Strittmatter <daniel@smattr.de>
2016-01-03 18:53:57 +0100

    Fix typo in sample

--- samples/Search/SearchIssues.hs
+++ samples/Search/SearchIssues.hs
@@ -5,7 +5,7 @@ import qualified Github.Search as Github
 import Control.Monad (forM_)
 
 main = do
-  let query = "q=build%2Arepo%3Aphadej%2Fgithub&per_page=100"
+  let query = "q=build%20repo%3Aphadej%2Fgithub&per_page=100"
   let auth = Nothing
   result <- Github.searchIssues' auth query
   case result of
