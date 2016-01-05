7c5f68124262a86093d07a844240b0bd41b90795
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-01-06 00:07:12 +0200

    Add git data to Github.All

--- Github/GitData/Blobs.hs
+++ Github/GitData/Blobs.hs
@@ -1,24 +1,31 @@
 -- | The API for dealing with git blobs from Github repos, as described in
 -- <http://developer.github.com/v3/git/blobs/>.
 module Github.GitData.Blobs (
- blob
-,blob'
-,module Github.Data
-) where
+    blob,
+    blob',
+    blobR,
+    module Github.Data,
+    ) where
 
+import Github.Auth
 import Github.Data
-import Github.Private
+import Github.Request
 
 -- | Get a blob by SHA1.
 --
 -- > blob' (Just ("github-username", "github-password")) "thoughtbot" "paperclip" "bc5c51d1ece1ee45f94b056a0f5a1674d7e8cba9"
-blob' :: Maybe GithubAuth -> String -> String -> String -> IO (Either Error Blob)
-blob' auth user reqRepoName sha =
-  githubGet' auth ["repos", user, reqRepoName, "git", "blobs", sha]
-
+blob' :: Maybe GithubAuth -> Name GithubOwner -> Name Repo -> Name Blob -> IO (Either Error Blob)
+blob' auth user repo sha =
+    executeRequestMaybe auth $ blobR user repo sha
 
 -- | Get a blob by SHA1.
 --
 -- > blob "thoughtbot" "paperclip" "bc5c51d1ece1ee45f94b056a0f5a1674d7e8cba9"
-blob :: String -> String -> String -> IO (Either Error Blob)
+blob :: Name GithubOwner -> Name Repo -> Name Blob -> IO (Either Error Blob)
 blob = blob' Nothing
+
+-- | Get a blob.
+-- See <https://developer.github.com/v3/git/blobs/#get-a-blob>
+blobR :: Name GithubOwner -> Name Repo -> Name Blob -> GithubRequest k Blob
+blobR user repo sha =
+    GithubGet ["repos", untagName user, untagName repo, "git", "blobs", untagName sha] ""
