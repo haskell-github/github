2ae1c419fcb28fd262ee35389384722b8cf7957b
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-01-06 21:18:27 +0200

    Move Auth data definition to Github.Auth

--- Github/Auth.hs
+++ Github/Auth.hs
@@ -1,4 +1,19 @@
--- | The Github auth data type
-module Github.Auth (P.GithubAuth(..)) where
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric      #-}
+module Github.Auth where
 
-import qualified Github.Private as P
+import Control.DeepSeq (NFData)
+import Data.Data       (Data, Typeable)
+import GHC.Generics    (Generic)
+
+import qualified Data.ByteString as BS
+
+-- | The Github auth data type
+data GithubAuth = GithubBasicAuth BS.ByteString BS.ByteString
+                | GithubOAuth String -- ^ token
+                | GithubEnterpriseOAuth String  -- custom API endpoint without
+                                                -- trailing slash
+                                        String  -- token
+                deriving (Show, Data, Typeable, Eq, Ord, Generic)
+
+instance NFData GithubAuth
