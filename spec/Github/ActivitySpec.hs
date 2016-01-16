32a1d6664ae893fd5be13751d7d69533f01e931c
Oleg Grenrus <oleg.grenrus@iki.fi>
2016-01-16 12:08:41 +0200

    Add Activity spec

---
+++ spec/Github/ActivitySpec.hs
@@ -0,0 +1,32 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell   #-}
+module Github.ActivitySpec where
+
+import Github.Auth          (GithubAuth (..))
+import Github.Activity.Watching (watchersForR)
+import Github.Request       (executeRequest)
+
+import Data.Either.Compat (isRight)
+import System.Environment (lookupEnv)
+import Test.Hspec         (Spec, describe, it, pendingWith, shouldSatisfy)
+
+import qualified Data.Vector as V
+
+fromRightS :: Show a => Either a b -> b
+fromRightS (Right b) = b
+fromRightS (Left a) = error $ "Expected a Right and got a Left" ++ show a
+
+withAuth :: (GithubAuth -> IO ()) -> IO ()
+withAuth action = do
+  mtoken <- lookupEnv "GITHUB_TOKEN"
+  case mtoken of
+    Nothing    -> pendingWith "no GITHUB_TOKEN"
+    Just token -> action (GithubOAuth token)
+
+spec :: Spec
+spec = do
+  describe "watchersForR" $ do
+    it "works" $ withAuth $ \auth -> do
+      cs <- executeRequest auth $ watchersForR "phadej" "github" Nothing
+      cs `shouldSatisfy` isRight
+      V.length (fromRightS cs) `shouldSatisfy` (> 10)
