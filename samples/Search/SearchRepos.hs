66f3838af3b178541890411c8245e3cdfe7c2a85
waddlaw <ingronze@gmail.com>
2017-02-15 18:16:31 +0000

    Update sample code

--- samples/Search/SearchRepos.hs
+++ samples/Search/SearchRepos.hs
@@ -1,33 +1,37 @@
 {-# LANGUAGE OverloadedStrings #-}
-module SearchRepos where
+module Main (main) where
 
-import qualified Github.Search as Github
-import qualified Github.Data as Github
-import Control.Monad (forM,forM_)
-import Data.Maybe (fromMaybe)
-import Data.List (intercalate)
+import qualified GitHub.Data.Search as Github (searchResultTotalCount, searchResultResults)
+import qualified GitHub.Data.Repos as Github (Repo(..))
+import qualified GitHub.Endpoints.Search as Github (searchRepos')
+import Control.Monad (forM_)
 import System.Environment (getArgs)
 import Text.Printf (printf)
-import Data.Time.Clock (getCurrentTime, UTCTime(..))
-import Data.Time.LocalTime (utc,utcToLocalTime,localDay,localTimeOfDay,TimeOfDay(..))
+import Data.Time.Clock (getCurrentTime)
+import Data.Time.LocalTime (utc,utcToLocalTime,localDay)
 import Data.Time.Calendar (toGregorian)
 
+import GitHub.Internal.Prelude (pack)
+
+
+main :: IO ()
 main = do
   args <- getArgs
   date <- case args of
             (x:_)     -> return x
             otherwise -> today
-  let query = "q=language%3Ahaskell created%3A>" ++ date ++ "&per_page=100"
+  let qs = [("q", "language:haskell created:<" `mappend` pack date), ("per_page", "100"), ("page", "2")]
   let auth = Nothing
-  result <- Github.searchRepos' auth query
+  result <- Github.searchRepos' auth qs
   case result of
     Left e  -> putStrLn $ "Error: " ++ show e
-    Right r -> do forM_ (Github.searchReposRepos r) (\r -> do
+    Right r -> do forM_ (Github.searchResultResults r) (\r -> do
                     putStrLn $ formatRepo r
                     putStrLn ""
                     )
                   putStrLn $ "Count: " ++ show n ++ " Haskell repos created since " ++ date
-      where n = Github.searchReposTotalCount r
+                  putStrLn $ show $ length $ Github.searchResultResults r
+      where n = Github.searchResultTotalCount r
 
 -- | return today (in UTC) formatted as YYYY-MM-DD
 today :: IO String
@@ -38,19 +42,4 @@ today = do
    in return $ printf "%d-%02d-%02d" y m d
 
 formatRepo :: Github.Repo -> String
-formatRepo r =
-  let fields = [ ("Name", Github.repoName)
-                 ,("URL",  Github.repoHtmlUrl)
-                 ,("Description", orEmpty . Github.repoDescription)
-                 ,("Created-At", formatMaybeDate . Github.repoCreatedAt)
-                 ,("Pushed-At", formatMaybeDate . Github.repoPushedAt)
-                 ,("Stars", show . Github.repoStargazersCount)
-               ]
-  in intercalate "\n" $ map fmt fields
-    where fmt (s,f) = fill 12 (s ++ ":") ++ " " ++ f r
-          orEmpty = fromMaybe ""
-          fill n s = s ++ replicate n' ' '
-            where n' = max 0 (n - length s) 
-
-formatMaybeDate = maybe "???" formatDate
-formatDate = show . Github.fromDate
+formatRepo = show
