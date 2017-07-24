80b8e69702dcd0c8f60b1f771ba1b86dbd1f8193
Scott Fleischman <scott-fleischman@users.noreply.github.com>
2017-07-24 11:29:22 -0700

    Add create/update/delete example.

--- samples/Repos/Contents.hs
+++ samples/Repos/Contents.hs
@@ -5,8 +5,10 @@ module Main where
 
 import           Common                          hiding
                  (getContents, intercalate, take, truncate, unlines)
+import qualified Data.ByteString.Base64          as Base64
 import           Data.Text
-                 (Text, intercalate, pack, take, unlines)
+                 (Text, intercalate, take, unlines)
+import           Data.Text.Encoding              (decodeUtf8, encodeUtf8)
 import           Data.Text.IO                    (putStrLn)
 import qualified Data.Vector                     as Vector
 import qualified GitHub.Data                     as GitHub
@@ -21,6 +23,8 @@ main = do
   putStrLn "LICENSE"
   putStrLn "======="
   getContents "LICENSE"
+
+  createUpdateDeleteSampleFile
 
 getContents :: Text -> IO ()
 getContents path = do
@@ -50,9 +54,63 @@ formatContentInfo contentInfo =
     , "html url: " <> (GitHub.getUrl . GitHub.contentHtmlUrl) contentInfo
     ]
 
+formatItem :: GitHub.ContentItem -> Text
 formatItem item =
    "type: " <> tshow (GitHub.contentItemType item) <> "\n" <>
   formatContentInfo (GitHub.contentItemInfo item)
 
-
+truncate :: Text -> Text
 truncate str = take 40 str <> "... (truncated)"
+
+createUpdateDeleteSampleFile :: IO ()
+createUpdateDeleteSampleFile = do
+  let
+    auth = GitHub.OAuth "oauthtoken"
+    owner = "repoOwner"
+    repo = "repoName"
+    author = GitHub.Author
+      { GitHub.authorName = "John Doe"
+      , GitHub.authorEmail = "johndoe@example.com"
+      }
+    defaultBranch = Nothing
+    base64Encode = decodeUtf8 . Base64.encode . encodeUtf8
+  createResult <- failOnError $ GitHub.createFile auth owner repo
+    GitHub.CreateFile
+    { GitHub.createFilePath      = "sample.txt"
+    , GitHub.createFileMessage   = "Add sample.txt"
+    , GitHub.createFileContent   = base64Encode "Hello"
+    , GitHub.createFileBranch    = defaultBranch
+    , GitHub.createFileAuthor    = Just author
+    , GitHub.createFileCommitter = Just author
+    }
+
+  let getResultSHA = GitHub.contentSha . GitHub.contentResultInfo . GitHub.contentResultContent
+  let createFileSHA = getResultSHA createResult
+  updateResult <- failOnError $ GitHub.updateFile auth owner repo
+    GitHub.UpdateFile
+    { GitHub.updateFilePath      = "sample.txt"
+    , GitHub.updateFileMessage   = "Update sample.txt"
+    , GitHub.updateFileContent   = base64Encode "Hello world!"
+    , GitHub.updateFileSHA       = createFileSHA
+    , GitHub.updateFileBranch    = defaultBranch
+    , GitHub.updateFileAuthor    = Just author
+    , GitHub.updateFileCommitter = Just author
+    }
+
+  let updateFileSHA = getResultSHA updateResult
+  failOnError $ GitHub.deleteFile auth owner repo
+    GitHub.DeleteFile
+    { GitHub.deleteFilePath      = "sample.txt"
+    , GitHub.deleteFileMessage   = "Delete sample.txt"
+    , GitHub.deleteFileSHA       = updateFileSHA
+    , GitHub.deleteFileBranch    = defaultBranch
+    , GitHub.deleteFileAuthor    = Just author
+    , GitHub.deleteFileCommitter = Just author
+    }
+
+failOnError :: IO (Either GitHub.Error a) -> IO a
+failOnError c = c >>= go
+  where
+  go r = case r of
+    Left err -> fail . show $ err
+    Right x -> return x
