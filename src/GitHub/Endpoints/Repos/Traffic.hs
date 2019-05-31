018c8bd2385a9f9bc146938356cd6ba88e67186b
Andrew McMiddlin <andrew@ajmccluskey.com>
2019-05-31 13:05:13 +1000

    Start traffic endpoint.

---
+++ src/GitHub/Endpoints/Repos/Traffic.hs
@@ -0,0 +1,49 @@
+-- | The traffic API, as described at <https://developer.github.com/v3/repos/traffic/>
+module GitHub.Endpoints.Repos.Traffic () where
+
+import Data.Vector (Vector)
+
+import GitHub.Data (Referrer, Name, Repo)
+
+-- | The top 10 referrers for the past 14 days.
+--
+-- > popularReferrers "qfpl" "tasty-hedgehog"
+popularReferrers :: Name Owner -> Name Repo -> IO (Either Error (Vector Referrer))
+popularReferrers =
+  popularReferrers' Nothing
+
+-- | The top 10 referrers for the past 14 days.
+-- | With authentication.
+--
+-- > popularReferrers' (Just $ BasicAuth "github-username" "github-password") "qfpl" "tasty-hedgehog"
+popularReferrers' :: Maybe Auth -> Name Owner -> Name Repo -> IO (Either Error (Vector Referrer))
+popularReferrers' auth user repo =
+  executeRequestMaybe auth $ popularReferrersR user repo
+
+popularReferrersR :: Name Owner -> Name Repo -> Request k (Vector Referrer)
+popularReferrersR user repo =
+  query ["repos", toPathPart user, toPathPart repo, "traffic", "popular", "referrers"] []
+
+popularPaths :: Name Owner -> Name Repo -> IO (Either Error (Vector Path))
+popularPaths =
+  undefined
+
+popularPaths' :: Maybe Auth -> Name Owner -> Name Repo -> IO (Either Error (Vector Path))
+popularPaths' =
+  undefined
+
+views :: Name Owner -> Name Repo -> Period -> IO (Either Error Views)
+views =
+  undefined
+
+views' :: Maybe Auth -> Name Owner -> Name Repo -> Period -> IO (Either Error Views)
+views' =
+  undefined
+
+clones :: Name Owner -> Name Repo -> Period -> IO (Either Error Clones)
+clones =
+  undefined
+
+clones' :: Maybe Auth -> Name Owner -> Name Repo -> Period -> IO (Either Error Clones)
+clones' =
+  undefined
