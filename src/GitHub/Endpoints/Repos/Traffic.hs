78f80122b848190934d3609b8b4cd2f82bd2c364
Andrew McMiddlin <andrew@ajmccluskey.com>
2019-06-03 07:53:59 +1000

    Remove functions that don't take auth.
    Traffic API requires auth, so remove the functions that don't do auth. The ones
    you cargo culted from some other module when getting started...

--- src/GitHub/Endpoints/Repos/Traffic.hs
+++ src/GitHub/Endpoints/Repos/Traffic.hs
@@ -1,15 +1,11 @@
 -- | The traffic API, as described at <https://developer.github.com/v3/repos/traffic/>
 module GitHub.Endpoints.Repos.Traffic (
-    popularReferrers,
     popularReferrers',
     popularReferrersR,
-    popularPaths,
     popularPaths',
     popularPathsR,
-    views,
     views',
     viewsR,
-    clones,
     clones',
     clonesR
     ) where
@@ -18,58 +14,39 @@ import Data.Vector (Vector)
 
 import GitHub.Data (Referrer, Name, Repo, Owner, Auth, Error, Clones, PopularPath, Period, Views, prettyPeriod)
 import GitHub.Data.Request (query, toPathPart)
-import GitHub.Request (Request, executeRequestMaybe)
-
--- | The top 10 referrers for the past 14 days.
---
--- > popularReferrers "qfpl" "tasty-hedgehog"
-popularReferrers :: Name Owner -> Name Repo -> IO (Either Error (Vector Referrer))
-popularReferrers =
-  popularReferrers' Nothing
+import GitHub.Request (Request, executeRequest)
 
 -- | The top 10 referrers for the past 14 days.
 -- | With authentication.
 --
 -- > popularReferrers' (Just $ BasicAuth "github-username" "github-password") "qfpl" "tasty-hedgehog"
-popularReferrers' :: Maybe Auth -> Name Owner -> Name Repo -> IO (Either Error (Vector Referrer))
+popularReferrers' :: Auth -> Name Owner -> Name Repo -> IO (Either Error (Vector Referrer))
 popularReferrers' auth user =
-  executeRequestMaybe auth . popularReferrersR user
+  executeRequest auth . popularReferrersR user
 
 popularReferrersR :: Name Owner -> Name Repo -> Request k (Vector Referrer)
 popularReferrersR user repo =
   query ["repos", toPathPart user, toPathPart repo, "traffic", "popular", "referrers"] []
 
-popularPaths :: Name Owner -> Name Repo -> IO (Either Error (Vector PopularPath))
-popularPaths =
-  popularPaths' Nothing
-
-popularPaths' :: Maybe Auth -> Name Owner -> Name Repo -> IO (Either Error (Vector PopularPath))
+popularPaths' :: Auth -> Name Owner -> Name Repo -> IO (Either Error (Vector PopularPath))
 popularPaths' auth user =
-  executeRequestMaybe auth . popularPathsR user
+  executeRequest auth . popularPathsR user
 
 popularPathsR :: Name Owner -> Name Repo -> Request k (Vector PopularPath)
 popularPathsR user repo =
   query ["repos", toPathPart user, toPathPart repo, "traffic", "popular", "paths"] []
 
-views :: Name Owner -> Name Repo -> Period p -> IO (Either Error (Views p))
-views =
-  views' Nothing
-
-views' :: Maybe Auth -> Name Owner -> Name Repo -> Period p -> IO (Either Error (Views p))
+views' :: Auth -> Name Owner -> Name Repo -> Period p -> IO (Either Error (Views p))
 views' auth user repo =
-  executeRequestMaybe auth . viewsR user repo
+  executeRequest auth . viewsR user repo
 
 viewsR :: Name Owner -> Name Repo -> Period p -> Request k (Views p)
 viewsR user repo period =
   query ["repos", toPathPart user, toPathPart repo, "traffic", "views"] [("per", Just $ prettyPeriod period)]
 
-clones :: Name Owner -> Name Repo -> Period p -> IO (Either Error (Clones p))
-clones =
-  clones' Nothing
-
-clones' :: Maybe Auth -> Name Owner -> Name Repo -> Period p -> IO (Either Error (Clones p))
+clones' :: Auth -> Name Owner -> Name Repo -> Period p -> IO (Either Error (Clones p))
 clones' auth user repo =
-  executeRequestMaybe auth . clonesR user repo
+  executeRequest auth . clonesR user repo
 
 clonesR :: Name Owner -> Name Repo -> Period p -> Request k (Clones p)
 clonesR user repo period =
