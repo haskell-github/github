5aba76f286ef75352562b964d2cab869fc6c6e36
Andrew McMiddlin <andrew@ajmccluskey.com>
2019-06-05 20:48:13 +1000

    Working nix (at least nix-build).

--- default.nix
+++ default.nix
@@ -2,15 +2,35 @@
 , dev ? false
 }:
 let
-  pkgs = import nixpkgs { config.allowBroken = true; };
-  hp = pkgs.haskellPackages.override (old: {
-    overrides = pkgs.lib.composeExtensions (old.overrides or (_: _: {})) (self: super: with pkgs.haskell.lib; {
-      binary-instances-1 = doJailbreak self.binary-instances-1;
-      QuickCheck = self.QuickCheck_2_13_1;
+  pkgs = import nixpkgs {};
+  hp = pkgs.haskellPackages.override (with pkgs.haskell.lib; {
+    overrides = self: super: {
+      # Newer versions of things we need
+      ansi-terminal = super.ansi-terminal_0_9_1;
+      binary-orphans = super.binary-orphans_1_0_1;
+      QuickCheck = super.QuickCheck_2_13_1;
       quickcheck-instances = super.quickcheck-instances_0_3_21;
-      binary-orphans = self.binary-orphans_1_0_1;
-      github = super.callCabal2nix "github" ./github.cabal {};
-    });
+      tasty = super.tasty_1_2_2;
+      time-compat = super.time-compat_1_9_2_2;
+      unordered-containers = super.unordered-containers_0_2_10_0;
+
+      # Things that work with our newer versions but don't know it yet
+      # hspec test failure looks like it relies on some RNG to be just
+      # right, not critical
+      ChasingBottoms = doJailbreak super.ChasingBottoms;
+      hspec-core = dontCheck (doJailbreak super.hspec-core);
+      optparse-applicative = doJailbreak super.optparse-applicative;
+
+      # We subbed in the correct versions of things it needs to work
+      binary-instances = overrideCabal super.binary-instances (drv: {
+        broken = false;
+      });
+
+      # Break infinite recursion through QuickCheck test dep
+      splitmix = dontCheck super.splitmix;
+
+    };
   });
+  github = hp.callCabal2nix "github" ./. {};
 in
-  hp.github
+  github
--- shell.nix
+++ shell.nix
@@ -1,14 +1,3 @@
 {nixpkgs ? import ./nix/nixpkgs.nix}:
 
 (import ./. {inherit nixpkgs;}).env
-# let
-#   pkgs = import nixpkgs {};
-#   ghcWithP = pkgs.haskellPackages.ghcWithPackages (hp: with hp; [
-#     pkgs.cabal-install
-#     ghcid
-#     zlib
-#   ]);
-# in
-#   pkgs.mkShell {
-#     buildInputs = [ghcWithP];
-#   }
