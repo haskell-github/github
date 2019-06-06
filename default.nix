2709bf3d7793eb7c710afb8b415855e20789ad32
Andrew McMiddlin <andrew@ajmccluskey.com>
2019-06-06 15:59:36 +1000

    Add nixpkgs argument to default.nix, pinning it by default.

--- default.nix
+++ default.nix
@@ -1,3 +1,4 @@
+{ nixpkgs ? import ./nixpkgs.nix }:
 let
-  pkgs = import <nixpkgs> { overlays = [ (import ./overlay.nix) ]; };
+  pkgs = import nixpkgs { overlays = [ (import ./overlay.nix) ]; };
 in pkgs.haskellPackages.callCabal2nix "github" ./. { }
