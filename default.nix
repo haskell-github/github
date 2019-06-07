a15e05f7082c2147ad457f98ef8ace3784284f81
Andrew McMiddlin <andrew@ajmccluskey.com>
2019-06-07 11:03:31 +1000

    Add openssl dependencies required for cabal v2-*.
    These deps are behind a flag, so not included by cabal2nix. The flag is enabled
    in the cabal.project (i.e. for v2 builds), so add the dependencies to the
    derivation manually.

--- default.nix
+++ default.nix
@@ -1,4 +1,14 @@
 { nixpkgs ? import ./nixpkgs.nix }:
 let
   pkgs = import nixpkgs { overlays = [ (import ./overlay.nix) ]; };
-in pkgs.haskellPackages.callCabal2nix "github" ./. { }
+  github = pkgs.haskellPackages.callCabal2nix "github" ./. { };
+  githubOpenSSL = pkgs.haskell.lib.overrideCabal github (drv: {
+    libraryHaskellDepends = drv.libraryHaskellDepends ++ (with pkgs.haskellPackages; [
+      # These are behind a flag that that is enabled in the `cabal.project` file.
+      http-client-openssl
+      HsOpenSSL
+      HsOpenSSL-x509-system
+    ]);
+  });
+in
+  githubOpenSSL
