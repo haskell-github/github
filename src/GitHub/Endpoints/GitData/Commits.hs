3959e1cb7bafa98c038e294e7593d5e713f65fd5
Gaëtan André <rvlander@gaetanandre.eu>
2019-08-25 13:38:07 +0200

    add get commit with auth

--- src/GitHub/Endpoints/GitData/Commits.hs
+++ src/GitHub/Endpoints/GitData/Commits.hs
@@ -7,6 +7,7 @@
 -- <http://developer.github.com/v3/git/commits/>.
 module GitHub.Endpoints.GitData.Commits (
     commit,
+    commit',
     gitCommitR,
     module GitHub.Data,
 ) where
@@ -18,10 +19,16 @@ import Prelude ()
 
 -- | A single commit, by SHA1.
 --
+
+-- > reference' (Just $ BasicAuth "github-username" "github-password") "mike-burns" "github" "heads/master"
+commit' :: Maybe Auth -> Name Owner -> Name Repo -> Name GitCommit -> IO (Either Error GitCommit)
+commit' auth user repo sha =
+    executeRequestMaybe auth $ gitCommitR user repo sha
+
 -- > commit "thoughtbot" "paperclip" "bc5c51d1ece1ee45f94b056a0f5a1674d7e8cba9"
 commit :: Name Owner -> Name Repo -> Name GitCommit -> IO (Either Error GitCommit)
-commit user repo sha =
-    executeRequest' $ gitCommitR user repo sha
+commit =
+    commit' Nothing
 
 -- | Query a commit.
 -- See <https://developer.github.com/v3/git/commits/#get-a-commit>
