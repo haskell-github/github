1a58c69d9456122c2c6dd5f08173393801ddd730
sanjiv sahayam <sanjsmailbox@gmail.com>
2020-08-03 09:45:47 +1000

    Fix typo

--- spec/GitHub/ReviewDecodeSpec.hs
+++ spec/GitHub/ReviewDecodeSpec.hs
@@ -15,7 +15,7 @@ spec = do
   describe "PENDING state" $ do
     -- https://docs.github.com/en/rest/reference/pulls#create-a-review-for-a-pull-request
     -- > Pull request reviews created in the PENDING state do not include the submitted_at property in the response.
-    it "decodes review when in submitted_at is missing" $ do
+    it "decodes review when submitted_at is missing" $ do
       let reviewInfo = eitherDecodeStrict $(embedFile "fixtures/pull-request-pending-review.json") :: Either String Review
       reviewInfo `shouldSatisfy` isRight
 
