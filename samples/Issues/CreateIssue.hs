922c822d8c72c004695eef022352288be58d3cf6
Andreas Abel <andreas.abel@ifi.lmu.de>
2022-04-19 16:25:10 +0200

    Update CreateIssue.hs to request model & OAuth; add cabal entry (#195)
    Can be run as follows:
    
        samples/$ GITHUB_TOKEN=<<my token>> cabal run github-create-issue

--- samples/Issues/CreateIssue.hs
+++ samples/Issues/CreateIssue.hs
@@ -1,36 +1,53 @@
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE OverloadedStrings #-}
 
-module CreateIssue where
-
-import qualified Data.Text               as T (unpack)
+import           Data.String                       (fromString)
+import qualified Data.Text               as Text   (unpack)
 import qualified Data.Vector             as Vector (fromList)
 import qualified GitHub.Auth             as GitHub
 import qualified GitHub.Data.Issues      as GitHub
 import qualified GitHub.Endpoints.Issues as GitHub
+import qualified GitHub.Request          as GitHub
 
+import           System.Environment                (lookupEnv)
+import qualified System.Exit             as Exit   (die)
+
+self :: String
+self = "github-create-issue"
 
 main :: IO ()
 main = do
-  let auth = GitHub.BasicAuth "user" "password"
-      newiss = (GitHub.newIssue "A new issue")
-        { GitHub.newIssueBody = Just "Issue description text goes here"
-        -- UnComment to add issue labels.
-        -- , GitHub.newIssueLabels = Just $ Vector.fromList ["foo", "bar", "baz"]
+  token <- lookupEnv "GITHUB_TOKEN" >>= \case
+    Nothing    -> die "variable GITHUB_TOKEN not set"
+    Just token -> return $ fromString token
+
+  let auth    = GitHub.OAuth token
+      newiss  = (GitHub.newIssue "A new issue")
+        { GitHub.newIssueBody   = Just "Issue description text goes here"
+        , GitHub.newIssueLabels = Just $ Vector.fromList ["foo", "bar", "baz"]
         }
-  possibleIssue <- GitHub.createIssue auth "thoughtbot" "paperclip" newiss
-  putStrLn $ either (\e -> "Error: " ++ show e)
-                    formatIssue
-                    possibleIssue
+      request = GitHub.createIssueR "haskell-github" "playground" newiss
+
+  GitHub.github auth request >>= \case
+    Left  err   -> die $ show err
+    Right issue -> putStrLn $ formatIssue issue
+
+die :: String -> IO a
+die msg = Exit.die $ concat [ self, ": Error: ", msg ]
 
 formatIssue :: GitHub.Issue -> String
-formatIssue issue =
-  formatUser issue ++
-    " opened this issue " ++
-    (show $ GitHub.issueCreatedAt issue) ++ "\n" ++
-    (T.unpack $ GitHub.issueState issue) ++ " with " ++
-    (show $ GitHub.issueComments issue) ++ " comments" ++ "\n\n" ++
-    (T.unpack $ GitHub.issueTitle issue)
+formatIssue issue = concat
+  [ formatUser issue
+  , " opened this issue "
+  , show $ GitHub.issueCreatedAt issue
+  , "\n"
+  , show $ GitHub.issueState issue
+  , " with "
+  , show $ GitHub.issueComments issue
+  , " comments\n\n"
+  , Text.unpack $ GitHub.issueTitle issue
+  ]
 
 formatUser :: GitHub.Issue -> String
 formatUser issue =
-  (T.unpack . GitHub.untagName . GitHub.simpleUserLogin $ GitHub.issueUser issue)
+  Text.unpack . GitHub.untagName . GitHub.simpleUserLogin $ GitHub.issueUser issue
